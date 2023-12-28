b40c90cf41c74d6308ce5111f6f38c935b77ba06
ab-h-i-n <abhinshelly@gmail.com>
2023-12-28 17:55:39 +0530

    .

--- BrightnessSlider.js
+++ BrightnessSlider.js
@@ -1,14 +1,39 @@
-const slider = document.querySelector('.slider');
+var slider = document.getElementById("slider");
+var track = document.getElementById("track");
+var isDragging = false;
 
-slider.addEventListener('input', () => {
-    console.log(slider.value);
-    updateThumbWidth(slider);
+window.onload = () =>{
+    track.style.width = "50%";
+}
+
+slider.addEventListener("mousedown", function(event) {
+  isDragging = true;
+  updateTrackWidth(event.clientX);
 });
 
-function updateThumbWidth(slider) {
-    // Calculate the width based on the slider's value
-    const thumbWidth = slider.value + '%';
+document.addEventListener("mousemove", function(event) {
+  if (isDragging) {
+    updateTrackWidth(event.clientX);
+  }
+});
 
-    // Apply the width to the thumb
-    slider.style.setProperty('--thumb-width', thumbWidth);
+document.addEventListener("mouseup", function() {
+  isDragging = false;
+});
+
+function updateTrackWidth(x) {
+  var sliderRect = slider.getBoundingClientRect();
+  var newX = x - sliderRect.left;
+  var percentage = (newX / sliderRect.width) * 100;
+
+  if (percentage < 0) {
+    percentage = 0;
+  } else if (percentage > 100) {
+    percentage = 100;
+  }
+
+  track.style.width = percentage + "%";
+
 }
+
+
--- statusShow.js
+++ statusShow.js
@@ -2,11 +2,13 @@
 const nav = document.querySelector('.nav');
 const statusBar = document.querySelector('.status_bar');
 const navDate = document.querySelector('.nav .date');
+const brightness = document.querySelector('.brightness-control');
 nav.addEventListener('click',handleEx);
 
 function handleEx(){
     statusBar.style.opacity = '100%';
     statusBar.style.height = '100%';
+    brightness.style.opacity = '100%';
     nav.removeEventListener('click',handleEx);
     nav.addEventListener('click',handleRe);
     navDate.style.opacity = '100%';
@@ -16,6 +18,7 @@ function handleRe() {
     statusBar.style.opacity = '0%';
     statusBar.style.height = '0%';
     navDate.style.opacity = '0%';
+    brightness.style.opacity = '0%';
     nav.removeEventListener('click', handleRe);
     nav.addEventListener('click', handleEx);
 }
