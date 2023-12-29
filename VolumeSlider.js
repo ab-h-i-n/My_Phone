c81cb14e7655aa95e51cac84c0b468354eae8889
ab-h-i-n <abhinshelly@gmail.com>
2023-12-29 13:01:33 +0530

    .

--- VolumeSlider.js
+++ VolumeSlider.js
@@ -1,36 +1,60 @@
-var slider = document.getElementById("vslider");
-var track = document.getElementById("vtrack");
+var slider = document.querySelector('.vertical-slider');
+var track = document.querySelector('.slider-thumb');
 var isDragging = false;
 
-window.onload = () => {
-    track.style.height = "50%";
+var volumeUp = document.querySelector('.volume_btn_1');
+var volumeDown = document.querySelector('.volume_btn_2');
+
+window.onload = () =>{
+    track.style.height = '50%';
 }
 
-slider.addEventListener("mousedown", function (event) {
+volumeUp.addEventListener("click" , ()=>{
+    var currentPercentage = parseInt(track.style.height);
+    var newPercentage = currentPercentage + 5;
+    if(newPercentage > 100){
+        newPercentage = 100;
+    }
+    track.style.height = `${newPercentage}%`;
+});
+volumeDown.addEventListener("click" , ()=>{
+    var currentPercentage = parseInt(track.style.height);
+    var newPercentage = currentPercentage - 5;
+    if(newPercentage < 0){
+        newPercentage = 0;
+    }
+    track.style.height = `${newPercentage}%`;
+});
+
+slider.addEventListener("mousedown", function(event){
     isDragging = true;
     updateTrackHeight(event.clientY);
 });
 
-slider.addEventListener("mousemove", function (event) {
-    if (isDragging) {
+document.addEventListener("mouseup", function(){
+    isDragging = false;
+})
+
+document.addEventListener("mousemove", function(event){
+    if(isDragging){
         updateTrackHeight(event.clientY);
     }
-});
+})
 
-document.addEventListener("mouseup", function () {
-    isDragging = false;
-});
-
-function updateTrackHeight(y) {
+function updateTrackHeight(y){
     var sliderRect = slider.getBoundingClientRect();
-    var newY = sliderRect.bottom - y;  // Adjusted to be relative to the bottom
-    var percentage = (newY / sliderRect.height) * 100;
+    var newY = y - sliderRect.top;
+    var percentage = Math.round((newY / sliderRect.height) * 100);
 
-    if (percentage < 0) {
-        percentage = 0;
-    } else if (percentage > 100) {
-        percentage = 100;
+    //to get 100 at top
+    var invPercentage = (100 - percentage);
+
+    if(invPercentage < 0){
+        invPercentage = 0;
+    }else if(invPercentage > 100){
+        invPercentage = 100;
     }
 
-    track.style.height = percentage + "%";
+    track.style.height = invPercentage + '%';
 }
+
