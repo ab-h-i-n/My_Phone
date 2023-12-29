2e631c113e61da4db7b545607ddb264853790732
ab-h-i-n <abhinshelly@gmail.com>
2023-12-29 16:05:29 +0530

    .

--- powerButton.js
+++ powerButton.js
@@ -3,23 +3,32 @@ var powerScreen = document.querySelector('.power-screen');
 var powerMenu = document.querySelector('.power-menu');
 var clickAndHoldTimeout;
 
+
 powerBtn.addEventListener('mousedown', () => {
+    clickAndHoldTimeout = setTimeout(() => {
+        powerScreen.style.display = 'flex';
+        powerScreen.style.opacity = '1';
+        wasClicked = false; // Reset the flag in case of a click-and-hold
+    }, 3000); // 3000 milliseconds (3 seconds)
+});
+
+//for touch screen
+
+powerBtn.addEventListener('touchstart', (event) => {
+    event.preventDefault(); // Prevents the default touch behavior (e.g., scrolling)
+    
     clickAndHoldTimeout = setTimeout(() => {
         powerScreen.style.display = 'flex';
         powerScreen.style.opacity = '1';
     }, 3000); // 3000 milliseconds (3 seconds)
 });
 
-document.addEventListener('mousedown', (event) => {
-    // Check if the click is outside the power menu
-    if (!powerMenu.contains(event.target) && powerScreen.style.display === 'flex') {
-        powerScreen.style.display = 'none';
-        powerScreen.style.opacity = '0';
-    }
-});
 
-powerBtn.addEventListener('mouseup', () => {
-    clearTimeout(clickAndHoldTimeout);
-    powerScreen.style.display = 'none';
-    powerScreen.style.opacity = '0';
-});
+document.addEventListener('click',(event)=>{
+
+    if(powerScreen.contains(event.target) & !powerMenu.contains(event.target)){
+        powerScreen.style.opacity = '0';
+        powerScreen.style.display ='none';
+        clearTimeout(clickAndHoldTimeout);
+    }
+})
